bf528c3241d7871e6bcf0fa32b63bc10c01cbb86
MuhammadWaqas-W <74444671+MuhammadWaqas-W@users.noreply.github.com>
2024-03-16 23:36:33 +0500

    feat:initial commit

---
+++ @next/components/molecules/verification-work-content/verification-work-content.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Box, Grid, Typography } from '@mui/material';
+import Image from 'next/image';
+
+export const VerificationWorkContent = ({
+  heading = '',
+  paragraph = '',
+  image = '',
+}: any) => {
+  return (
+    <Grid container>
+      <Grid item xs={12} sm={12} md={6} lg={6}>
+        <Typography
+          variant="h3"
+          sx={{
+            color: 'primary.dark',
+            lineHeight: '40px',
+            py: 3,
+          }}
+        >
+          {heading}
+        </Typography>
+
+        <Typography
+          variant="h4"
+          sx={{
+            color: 'primary.light',
+            pb: 3,
+            lineHeight: '28px',
+          }}
+        >
+          {paragraph}
+        </Typography>
+      </Grid>
+      <Grid item xs={12} sm={12} md={6} lg={6}>
+        <Box
+          sx={{
+            justifyContent: { md: 'end', sm: 'center' },
+            display: 'flex',
+          }}
+        >
+          <Image src={image} alt="" />
+        </Box>
+      </Grid>
+    </Grid>
+  );
+};
